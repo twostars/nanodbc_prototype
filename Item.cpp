60daedcd4a3aa87392b13095cb6a10770f45d9e9
twostars <i.am.twostars@gmail.com>
2025-07-05 14:35:42 +1000

    Add .editorconfig
    Reformat existing files for consistency

--- Item.cpp
+++ Item.cpp
@@ -2,5 +2,5 @@
 
 // static member initialization.  Needs to be done here or there'll be linker errors
 std::map<std::string, Item::ItemBindingFunction> Item::ColumnBindings = std::map<std::string, Item::ItemBindingFunction>();
-std::set<std::string> Item::columnNames = std::set<std::string>{"Num", "strName"};
+std::set<std::string> Item::columnNames = std::set<std::string> { "Num", "strName" };
 bool Item::isBindInit = false;
--- Item.h
+++ Item.h
@@ -10,70 +10,70 @@
 class Item
 {
 public:
-    // static bindings
-    typedef void (*ItemBindingFunction)(Item& m, const nanodbc::result& result, short colIndex);
-    static std::map<std::string, ItemBindingFunction> ColumnBindings;
-    
-    
-    /// \brief jsonSchema column description
-    int         Num;
+	// static bindings
+	typedef void (*ItemBindingFunction)(Item& m, const nanodbc::result& result, short colIndex);
+	static std::map<std::string, ItemBindingFunction> ColumnBindings;
 
-    /// \brief jsonSchema column description
-    /// \note maxLength: jsonSchema.column.length
-    std::string Name;
 
-    Item()
-    {
-        // generate with a default val by type to avoid pulling garbage from memory
-        Num = 0;
-        Name = "";
-    }
+	/// \brief jsonSchema column description
+	int         Num;
 
-    /// \brief initiates the 
-    /// \note this would be called via a generated method that calls it for each model
-    static void InitBindings()
-    {
-        if (!isBindInit)
-        {
-            ColumnBindings[std::string("Num")] = Item::BindNum;
-            ColumnBindings[std::string("strName")] = Item::BindName;
-            isBindInit = true;
-        }
-    }
+	/// \brief jsonSchema column description
+	/// \note maxLength: jsonSchema.column.length
+	std::string Name;
 
-    /// \brief table name
-    static std::string TableName()
-    {
-        return "ITEM";
-    }
+	Item()
+	{
+		// generate with a default val by type to avoid pulling garbage from memory
+		Num = 0;
+		Name = "";
+	}
 
-    /// \brief returns a constant reference to the std::set of column names
-    ///
-    /// \note this set should not be manipulated
-    static const std::set<std::string>& ColumnNames()
-    {
-        return columnNames;
-    }
+	/// \brief initiates the 
+	/// \note this would be called via a generated method that calls it for each model
+	static void InitBindings()
+	{
+		if (!isBindInit)
+		{
+			ColumnBindings[std::string("Num")] = Item::BindNum;
+			ColumnBindings[std::string("strName")] = Item::BindName;
+			isBindInit = true;
+		}
+	}
 
-    /// \brief database type for the model
-    static std::string DbType()
-    {
-        return "GAME";
-    }
+	/// \brief table name
+	static std::string TableName()
+	{
+		return "ITEM";
+	}
 
-    /// \brief Generated binding function for Num
-    static void BindNum(Item& m, const nanodbc::result& result, short colIndex)
-    {
-        m.Num = result.get<int>(colIndex);
-    }
+	/// \brief returns a constant reference to the std::set of column names
+	///
+	/// \note this set should not be manipulated
+	static const std::set<std::string>& ColumnNames()
+	{
+		return columnNames;
+	}
 
-    /// \brief Generated binding function for Name
-    static void BindName(Item& m, const nanodbc::result& result, short colIndex)
-    {
-        m.Name = result.get<std::string>(colIndex);
-    }
-    
+	/// \brief database type for the model
+	static std::string DbType()
+	{
+		return "GAME";
+	}
+
+	/// \brief Generated binding function for Num
+	static void BindNum(Item& m, const nanodbc::result& result, short colIndex)
+	{
+		m.Num = result.get<int>(colIndex);
+	}
+
+	/// \brief Generated binding function for Name
+	static void BindName(Item& m, const nanodbc::result& result, short colIndex)
+	{
+		m.Name = result.get<std::string>(colIndex);
+	}
+
 private:
-    static std::set<std::string> columnNames;
-    static bool isBindInit;
+	static std::set<std::string> columnNames;
+	static bool isBindInit;
 };

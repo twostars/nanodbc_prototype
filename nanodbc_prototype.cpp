3b187113ea8c7dcca6c1ceaf9b6bc42a5185f62a
kenner <13474249+kenner2@users.noreply.github.com>
2025-07-08 11:37:35 -0700

    add fullmodel as submodule, import and use it

--- nanodbc_prototype.cpp
+++ nanodbc_prototype.cpp
@@ -1,12 +1,14 @@
 ﻿#include <iostream>
 #include <string>
+#include <optional>
 
 #include "DatabaseConnManager.h"
 #include "Item.h"
-#include "ItemBinder.h"
 #include "ModelRecordSet.h"
 #include "SqlBuilder.h"
 #include "nanodbc/nanodbc.h"
+
+import FullModel;
 
 int main()
 {
@@ -16,27 +18,27 @@ int main()
 		std::string user = "knight";
 		std::string password = "knight";
 		DatabaseConnManager::SetGameDsn(dsn, user, password);
-
-		SqlBuilder<Item> filter {};
+		
+		SqlBuilder<model::Item> filter {};
 		filter.SetSelectColumns({ "Num", "strName" });
 
 		// using an iterator
-		ModelRecordSet<Item> recordSet(filter);
+		ModelRecordSet<model::Item> recordSet(filter);
 		int i = 0;
 		while (recordSet.next() && i < 10)
 		{
-			Item item = recordSet.get();
-			std::cout << item.Num << ": " << item.Name << "\n";
+			model::Item item = recordSet.get();
+			std::cout << item.Number << ": " << item.Name << "\n";
 			i++;
 		}
 
 		// loading results in a single query; currently this functions the same way as the iterator above
 		// need to look more at nanodbc library functions to see if there's a way to fetch everything
 		// in one database trip or not
-		std::vector<Item> results = Model::BatchSelect<Item>(filter);
+		std::vector<model::Item> results = Model::BatchSelect<model::Item>(filter);
 		for (std::size_t ix = 0; ix < results.size() && ix < 10; ix++)
 		{
-			std::cout << results.at(ix).Num << ": " << results.at(ix).Name << "\n";
+			std::cout << results.at(ix).Number << ": " << results.at(ix).Name << "\n";
 		}
 	}
 	catch (nanodbc::database_error& ex)
